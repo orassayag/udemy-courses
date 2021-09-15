0c6e1343a71a57a59a7d763590e45f826a5f46b3
Or Assayag <orassayag@gmail.com>
2021-09-15 17:22:02 +0300

    update packages

--- src/utils/files/file.utils.js
+++ src/utils/files/file.utils.js
@@ -24,7 +24,7 @@ class FileUtils {
     }
 
     async removeDirectoryIfExists(targetPath) {
-        if (!await this.isPathExists(targetPath)) {
+        if (await this.isPathExists(targetPath)) {
             await fs.remove(targetPath);
         }
     }
